3377a3d1cadc9daf55c1c5eca1c7c48ba49acdae
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-16 08:01:53 +0100

    linked first module to node.js

--- node/myFirstNode.js
+++ node/myFirstNode.js
@@ -1,7 +1,10 @@
 const http = require('http'); //this is a module, we store the contents in a variable to call methods on later. 
+const dt = require('./myFirstModule'); //module I have made to return date
 
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.end('The date and time are currently: ' + 
+  dt.myDateTime());
+  res.end();
 }).listen(8080);
 
