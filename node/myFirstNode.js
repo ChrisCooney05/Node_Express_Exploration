0e314a19518a7f6eaecd6e71cf1d8748706bbf45
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-16 08:21:31 +0100

    updated comments for learing

--- node/myFirstNode.js
+++ node/myFirstNode.js
@@ -1,10 +1,12 @@
 const http = require('http'); //this is a module, we store the contents in a variable to call methods on later. 
 const dt = require('./myFirstModule'); //module I have made to return date
 
-http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+//.creatServer makes a new server object
+http.createServer(function(req, res) { // function executed when someone tries to 
+  //access the computer on port 8080
+  res.writeHead(200, {'Content-Type': 'text/html'});//if response should be html, HTTP header with correct content should be included.
   res.end('The date and time are currently: ' + 
-  dt.myDateTime());
-  res.end();
-}).listen(8000);
+  dt.myDateTime()); // write a response to client
+  res.end(); // end the response 
+}).listen(8080); // port the server object is listening to
 
