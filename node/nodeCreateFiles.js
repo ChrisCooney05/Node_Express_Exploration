2dd3b5bc3f0830a7c84fecdeb921b75ccde1e103
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-17 07:49:16 +0100

    open method practice

--- node/nodeCreateFiles.js
+++ node/nodeCreateFiles.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 
-fs.appendFile('myNewFile1.txt', 'Hello Content!', function (err) {
+fs.open('myNewFile2.txt', 'w', function(err) {
   if (err) throw err;
-  console.log('Saved!')
-});
-// appendFile appends content to a file, if it does not exist it makes a new file.
-// file name myNewFile1 content is Hello Content!
-// this ran with node makes the file/appends then logs that everything is saved.
+  console.log('Saved!');
+})
+
+/* makes a new file if a file does not exist 
+the w flag specifies that the file be opened for writing
+*/
