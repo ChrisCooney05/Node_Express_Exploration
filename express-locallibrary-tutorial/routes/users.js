209b685ef5d65ba84dd5948304499ccf8fc8d96b
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-18 08:49:17 +0100

    added my first route

--- express-locallibrary-tutorial/routes/users.js
+++ express-locallibrary-tutorial/routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/cool', function(req, res, next) {
+  res.send("You're so cool!");
+});
+//added my own route users/cool that displays my message
+
 module.exports = router;
