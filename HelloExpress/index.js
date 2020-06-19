911defca53c34be7bb9ece89280740e8e0add0b3
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-19 07:52:44 +0100

    .all method used

--- HelloExpress/index.js
+++ HelloExpress/index.js
@@ -9,6 +9,11 @@ app.post('/hello', (req, res) => {
   res.send('You just made a Post request to /hello!');
 });
 
+app.all('/test' (req, res) => {
+  res.send('HTTP method has no effect on this route');
+});
+//can take all forms of request
+
 app.listen(8080);
 
 //curl -X POST "http://localhost:3000/hello" with an open server can test post request
