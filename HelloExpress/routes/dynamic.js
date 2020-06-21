06870c067ee6c4125df151e4379fd6104c0068af
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-21 12:51:56 +0100

    testing pug with if/else statements to render view

--- HelloExpress/routes/dynamic.js
+++ HelloExpress/routes/dynamic.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('dynamic', {
-    name: 'TutorialsPoint',
-    url: "http://www.tutorialspoint.com"
+    user: {name: 'Chris', age: '29'}
   });
 });
+//without the user object, the dynamic page would shift to
+//a single line of text forwarding to /sign_up.
 
 module.exports = router;
