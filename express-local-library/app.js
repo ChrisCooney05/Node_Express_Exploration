abc5cdd4917f01197a0f012a8d079560c284b202
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-22 07:40:24 +0100

    fixed syntax issue

--- express-local-library/app.js
+++ express-local-library/app.js
@@ -14,7 +14,7 @@ const { brotliDecompress } = require('zlib');
 var mongoDB = 'mongodb+srv://ChrisCooney05:tonBSkzqHPGnPM1h@cluster0.rbrfg.mongodb.net/local_library?retryWrites=true&w=majority'
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
-brotliDecompress.on('error', console.error.bind(console, 'MongoDB connection error:');)
+db.on('error', console.error.bind(console, 'MongoDB connection error:');)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
