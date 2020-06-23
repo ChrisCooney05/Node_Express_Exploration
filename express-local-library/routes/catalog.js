e938da35c26f5e651d0799fbc39a0808c9b5e2ea
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-23 08:07:37 +0100

    added routes for /bookinstance(s)

--- express-local-library/routes/catalog.js
+++ express-local-library/routes/catalog.js
@@ -87,3 +87,47 @@ router.get("/genre/:id", genreController.genreDetail);
 
 // GET request for list of all Genre.
 router.get("/genres", genreController.genreList);
+
+/// BOOKINSTANCE ROUTES ///
+
+// GET request for creating a BookInstance. This must come before route that displays BookInstance (uses id).
+router.get(
+  "/bookinstance/create",
+  bookInstanceController.bookInstanceCreateGet
+);
+
+// POST request for creating BookInstance.
+router.post(
+  "/bookinstance/create",
+  bookInstanceController.bookInstanceCreatePost
+);
+
+// GET request to delete BookInstance.
+router.get(
+  "/bookinstance/:id/delete",
+  bookInstanceController.bookInstanceDeleteGet
+);
+
+// POST request to delete BookInstance.
+router.post(
+  "/bookinstance/:id/delete",
+  bookInstanceController.bookInstanceDeletePost
+);
+
+// GET request to update BookInstance.
+router.get(
+  "/bookinstance/:id/update",
+  bookInstanceController.bookInstanceUpdateGet
+);
+
+// POST request to update BookInstance.
+router.post(
+  "/bookinstance/:id/update",
+  bookInstanceController.bookInstanceUpdatePost
+);
+
+// GET request for one BookInstance.
+router.get("/bookinstance/:id", bookInstanceController.BookInstanceDetail);
+
+// GET request for list of all BookInstance.
+router.get("/bookinstances", bookInstanceController.bookInstanceList);
