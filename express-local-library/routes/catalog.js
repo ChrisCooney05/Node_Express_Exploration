fc6e93cb90fead347a43420a945dff1e1d22e38e
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-23 07:39:08 +0100

    added routes for Genre(s)

--- express-local-library/routes/catalog.js
+++ express-local-library/routes/catalog.js
@@ -28,10 +28,62 @@ router.post("/book/:id/delete", bookController.bookDeletePost);
 router.get("/book/:id/update", bookController.bookUpdateGet);
 
 // POST request to update Book.
-router.get("/book/:id/update", bookController.bookUpdatePost);
+router.post("/book/:id/update", bookController.bookUpdatePost);
 
 // GET request for one Book.
 router.get("/book/:id", bookController.bookDetail);
 
 // GET request for list of all Book items.
 router.get("/books", bookController.bookList);
+
+/// AUTHOR ROUTES ///
+
+// GET request for creating Author. This must come before route for id (i.e. display author).
+router.get("/author/create", authorController.authorCreateGet);
+
+// POST request for creating Author.
+router.post("/author/create", authorController.authorCreatePost);
+
+// GET request to delete Author.
+router.get("/author/:id/delete", authorController.authorDeleteGet);
+
+// POST request to delete Author.
+router.post("/author/:id/delete", authorController.authorDeletePost);
+
+// GET request to update Author.
+router.get("./author/:id/update", authorController.authorUpdateGet);
+
+// POST request to update Author.
+router.post("/author/:id/update", authorController.authorUpdatePost);
+
+// GET request for one Author.
+router.get("/author/:id", authorController.authorDetail);
+
+// GET request for list of all Authors.
+router.get("/authors", authorController.authorList);
+
+/// GENRE ROUTES ///
+
+// GET request for creating a Genre. This must come before route that displays Genre (uses id).
+router.get("/genre/create", genreController.genreCreateGet);
+
+//POST request for creating Genre.
+router.post("/genre/create", genreController.genreCreatePost);
+
+// GET request to delete Genre.
+router.get("/genre/:id/delete", genreController.genreDeleteGet);
+
+// POST request to delete Genre.
+router.post("/genre/:id/delete", genreController.genreDeletePost);
+
+// GET request to update Genre.
+router.get("/genre/:id/update", genreController.genreUpdateGet);
+
+// POST request to update Genre.
+router.post("genre/:id/update", genreController.genreUpdatePost);
+
+// GET request for one Genre.
+router.get("/genre/:id", genreController.genreDetail);
+
+// GET request for list of all Genre.
+router.get("/genres", genreController.genreList);
