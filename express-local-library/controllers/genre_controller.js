2b5ada698c4b6b22864f6458d9aec3ea07b9383a
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-25 07:36:40 +0100

    added logic to genreDetail method

--- express-local-library/controllers/genre_controller.js
+++ express-local-library/controllers/genre_controller.js
@@ -1,4 +1,7 @@
 const Genre = require("../models/genre");
+const async = require("async");
+const book = require("../models/book");
+const { BookInstanceDetail } = require("./book_instance_controller");
 
 //display a list of Genre
 exports.genreList = function (req, res, next) {
@@ -17,8 +20,33 @@ exports.genreList = function (req, res, next) {
 };
 
 //display detail page for a specific Genre
-exports.genreDetail = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre detail: " + req.params.id);
+exports.genreDetail = function (req, res, next) {
+  async.parallel(
+    {
+      genre: function (callback) {
+        Genre.findById(req.params.id).exec(callback);
+      }, //finds genre via params id in url
+
+      genreBooks: function (callback) {
+        BookInstanceDetail.find({ genre: req.params.id }).exec(callback);
+      }, //finds all books that belong to that genre via the genre id
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.genre == null) {
+        let err = new Error("Genre not found");
+        err.status = 404;
+        return next(err); //we set up or own error if genre == nul as findById() returns successfully even if null
+      }
+      res.render("genre_detail", {
+        title: "Genre Details",
+        genre: results.genre,
+        genreBooks: results.genreBooks,
+      });
+    }
+  );
 };
 
 //display Genre create for on GET
