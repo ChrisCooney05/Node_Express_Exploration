3c6122d7d70ea27c73c1555050a78bb4c8c7d156
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-25 06:38:56 +0100

    used moment to format the date

--- express-local-library/models/book_instance.js
+++ express-local-library/models/book_instance.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const moment = require("moment");
 const Schema = mongoose.Schema;
 
 const BookInstanceSchema = new Schema({
@@ -18,6 +19,10 @@ const BookInstanceSchema = new Schema({
 //virtual for bookinstance's URL
 BookInstanceSchema.virtual("url").get(function () {
   return "/catalog/bookinstance/" + this._id;
+});
+
+BookInstanceSchema.virtual("DueBackFormatted").get(function () {
+  return moment(this.due_back).format("Do MMMM, YYYY");
 });
 
 module.exports = mongoose.model("BookInstance", BookInstanceSchema);
