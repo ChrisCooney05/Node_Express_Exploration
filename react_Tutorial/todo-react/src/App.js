5520cffa995ebe3789de739d60e3ebca9655e9c2
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-28 14:14:28 +0100

    worked a little with props and added a summary to App.js

--- react_Tutorial/todo-react/src/App.js
+++ react_Tutorial/todo-react/src/App.js
@@ -3,8 +3,10 @@ import logo from "./logo.svg";
 import "./App.css";
 //{} is how JSX (JavaScript XML) recognizes variables
 
-function App() {
-  const subject = "React";
+function App(props) {
+  //props shows as Object { subject: 'Chris'} props are collected into objects in this fashion
+  console.log(props);
+  const subject = props.subject;
   return (
     <div className="App">
       <header className="App-header">
@@ -16,3 +18,16 @@ function App() {
 }
 
 export default App;
+
+/*
+Components can import modules they need, and must export themselves at the bottom of their files.
+
+Component functions are named with PascalCase.
+
+You can read JSX variables by putting them between curly braces, like {so}.
+
+Some JSX attributes are different to HTML attributes, so that they don't conflict with JavaScript reserved words. 
+For example, class in HTML translates to className in JSX. Note that multi-word attributes are camel-cased.
+
+Props are written just like attributes inside component calls, and are passed into components.
+*/
