5664cbca193fa0e8e0e8dcfd6eeb13031776f8ef
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-29 06:47:56 +0100

    added ejs to file and first view created

--- javascript_server_and_api/index.js
+++ javascript_server_and_api/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 function handleRootRequest(req, res) {
-  res.send("Hello World");
+  res.render("home");
 } // function should be named to make it easier to track what our roots are doing
 //this should really be in another file
 
