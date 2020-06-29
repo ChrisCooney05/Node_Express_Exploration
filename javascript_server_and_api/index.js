1004998d0cd0abc043b6a6cfe5612858080f2111
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-29 06:41:56 +0100

    split / method out of root to improve readability

--- javascript_server_and_api/index.js
+++ javascript_server_and_api/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+function handleRootRequest(req, res) {
   res.send("Hello World");
-});
+} // function should be named to make it easier to track what our roots are doing
+//this should really be in another file
+
+app.get("/", handleRootRequest);
 
 app.listen("3000", () => {
   console.log("Listening on port 3000");
