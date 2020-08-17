971ab13589a4a47d2ee07cbe3318a9ca4b49fb31
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-17 16:34:50 +0100

    Cleaned up code and added info to CHALLENGE.md

--- santas_node_helper/index.js
+++ santas_node_helper/index.js
@@ -1,41 +1,57 @@
 const fs = require("fs");
 //PART ONE
 
-// fs.readFile("./moves.txt", (err, data) => {
-//   console.time("test");
-//   if (err) {
-//     console.log(err);
-//   }
-//   let floor = 0;
-//   let moves = data.toString();
-//   for (let i = 0; i < moves.length; i++) {
-//     if (moves[i] === "(") {
-//       floor++;
-//     } else {
-//       floor--;
-//     }
-//   }
-//   console.log(floor);
-//   console.timeEnd("test");
-// });
-
-fs.readFile("./moves.txt", (err, data) => {
-  console.time("test");
-  if (err) {
-    console.log(err);
-  }
-  let floor = 0;
-  let moves = data.toString();
-  for (let i = 0; i < moves.length; i++) {
-    if (moves[i] === "(") {
-      floor++;
-    } else {
-      floor--;
-      if (floor < 0) {
-        console.log(i + 1);
-        console.timeEnd("test");
-        return;
+function partOne() {
+  fs.readFile("./moves.txt", (err, data) => {
+    console.time("test");
+    let floor = 0;
+    let moves = data.toString();
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i] === "(") {
+        floor++;
+      } else {
+        floor--;
       }
     }
-  }
-});
+    console.log(floor);
+    console.timeEnd("test");
+  });
+}
+
+function partTwo() {
+  fs.readFile("./moves.txt", (err, data) => {
+    console.time("test2");
+    let floor = 0;
+    let moves = data.toString();
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i] === "(") {
+        floor++;
+      } else {
+        floor--;
+        if (floor < 0) {
+          console.log(i + 1);
+          console.timeEnd("test2");
+          return;
+        }
+      }
+    }
+  });
+}
+
+function partThree() {
+  fs.readFile("./moves.txt", (err, data) => {
+    console.time("test3");
+    let moves = data.toString();
+    let movesArray = moves.split("");
+    const answer = movesArray.reduce((acc, curVal) => {
+      if (curVal === "(") {
+        return (acc += 1);
+      } else {
+        return (acc -= 1);
+      }
+    }, 0);
+    console.log(answer);
+    console.timeEnd("test3");
+  });
+}
+partThree();
