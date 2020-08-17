fb7b6dee3ca3b78fc77ce2f03e38d2b62a9a74db
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-17 15:43:16 +0100

    Completed first part of the challenge, takes 7.166ms to complete

--- santas_node_helper/index.js
+++ santas_node_helper/index.js
@@ -1,8 +1,21 @@
 const fs = require("fs");
 
 fs.readFile("./moves.txt", (err, data) => {
+  console.time("test");
   if (err) {
     console.log(err);
   }
-  console.log(data.toString());
+  let up = 0;
+  let down = 0;
+  let moves = data.toString();
+  for (let i = 0; i < moves.length; i++) {
+    if (moves[i] === "(") {
+      up++;
+    } else {
+      down++;
+    }
+  }
+  let result = up - down;
+  console.log(result);
+  console.timeEnd("test");
 });
