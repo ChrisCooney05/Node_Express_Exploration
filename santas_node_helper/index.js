aa93b376880ebaf61565918d30d0b0f8a7441866
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-17 15:50:44 +0100

    Completed part two 6.500ms to complete and find the correct answer

--- santas_node_helper/index.js
+++ santas_node_helper/index.js
@@ -1,4 +1,23 @@
 const fs = require("fs");
+//PART ONE
+
+// fs.readFile("./moves.txt", (err, data) => {
+//   console.time("test");
+//   if (err) {
+//     console.log(err);
+//   }
+//   let floor = 0;
+//   let moves = data.toString();
+//   for (let i = 0; i < moves.length; i++) {
+//     if (moves[i] === "(") {
+//       floor++;
+//     } else {
+//       floor--;
+//     }
+//   }
+//   console.log(floor);
+//   console.timeEnd("test");
+// });
 
 fs.readFile("./moves.txt", (err, data) => {
   console.time("test");
@@ -12,8 +31,11 @@ fs.readFile("./moves.txt", (err, data) => {
       floor++;
     } else {
       floor--;
+      if (floor < 0) {
+        console.log(i + 1);
+        console.timeEnd("test");
+        return;
+      }
     }
   }
-  console.log(floor);
-  console.timeEnd("test");
 });
